ee43639e50fa37226e6cd69d14bb1f7ac50a126d
Jacob <gtkmvtyb25@gmail.com>
2018-12-19 23:50:29 +0300

    build for github pages

---
+++ todo/build/precache-manifest.ce3b7982accce8ef71a919fcca9ada24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d81eadffdf0807545b76",
+    "url": "/static/js/main.d81eadff.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "d81eadffdf0807545b76",
+    "url": "/static/css/main.83fc34eb.chunk.css"
+  },
+  {
+    "revision": "aa727fbcc7e93ff574b03ae985011dcd",
+    "url": "/index.html"
+  }
+];
