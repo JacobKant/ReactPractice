68f062fda1f2b27f6592a6e2db97a9326f61ddc9
Jacob <gtkmvtyb25@gmail.com>
2018-12-20 00:26:59 +0300

    +

---
+++ todo/build/precache-manifest.80d82f8fac5c96f57bc294811a9988be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "adeddb93e94b4f92013a",
+    "url": "/ReactPractice/todo/build/static/js/runtime~main.adeddb93.js"
+  },
+  {
+    "revision": "d81eadffdf0807545b76",
+    "url": "/ReactPractice/todo/build/static/js/main.d81eadff.chunk.js"
+  },
+  {
+    "revision": "f4926859e068d12e2815",
+    "url": "/ReactPractice/todo/build/static/js/1.f4926859.chunk.js"
+  },
+  {
+    "revision": "d81eadffdf0807545b76",
+    "url": "/ReactPractice/todo/build/static/css/main.83fc34eb.chunk.css"
+  },
+  {
+    "revision": "9fc8ae8e8e88b138e016485adcae4e96",
+    "url": "/ReactPractice/todo/build/index.html"
+  }
+];
--- todo/build/service-worker.js
+++ todo/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactPractice/precache-manifest.188987e269356964cb8e732e47f47d90.js"
+  "/ReactPractice/todo/build/precache-manifest.80d82f8fac5c96f57bc294811a9988be.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ReactPractice/index.html", {
+workbox.routing.registerNavigationRoute("/ReactPractice/todo/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
