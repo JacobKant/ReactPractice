bb1458928be86f925f9b18f18a7c61d627a37fa3
Jacob <gtkmvtyb25@gmail.com>
2018-12-23 23:33:38 +0300

    publish ghpages

---
+++ stardb/build/precache-manifest.50280d09dd08225df3cfa32a6163c39f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a03660ef1fefd625287",
+    "url": "/ReactPractice/stardb/build/static/js/runtime~main.8a03660e.js"
+  },
+  {
+    "revision": "c3bfad4906bb4941fab1",
+    "url": "/ReactPractice/stardb/build/static/js/main.c3bfad49.chunk.js"
+  },
+  {
+    "revision": "0572aae03b6f9854dd28",
+    "url": "/ReactPractice/stardb/build/static/js/1.0572aae0.chunk.js"
+  },
+  {
+    "revision": "c3bfad4906bb4941fab1",
+    "url": "/ReactPractice/stardb/build/static/css/main.497e4d7f.chunk.css"
+  },
+  {
+    "revision": "71eb6082d8e209bc3c8c409ca9c6b500",
+    "url": "/ReactPractice/stardb/build/index.html"
+  }
+];
