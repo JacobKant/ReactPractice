d0c255e127fef2a05b1e1a91ebea39a2f30457a8
JacobKantur <jkan1991@gmail.com>
2021-01-24 16:23:04 +0300

    format

--- stardb/src/services/swapi-service.js
+++ stardb/src/services/swapi-service.js
@@ -1,27 +1,27 @@
 export default class SwapiService {
-  _baseUrl = 'https://swapi.dev/api/';
+  _baseUrl = 'https://swapi.dev/api/'
 
   async getResource(resUrl) {
-    const res = await fetch(`${this._baseUrl}${resUrl}`);
+    const res = await fetch(`${this._baseUrl}${resUrl}`)
     if (!res.ok) {
-      throw new Error('Luke im your father');
+      throw new Error('Luke im your father')
     }
-    return await res.json();
+    return await res.json()
   }
 
   getAllPeople = async () => {
-    const res = await this.getResource('people/');
-    return res.results.map(this._transformPeople);
-  };
+    const res = await this.getResource('people/')
+    return res.results.map(this._transformPeople)
+  }
 
-  getPeople = async id => {
-    const people = await this.getResource(`people/${id}/`);
-    return this._transformPeople(people);
-  };
+  getPeople = async (id) => {
+    const people = await this.getResource(`people/${id}/`)
+    return this._transformPeople(people)
+  }
 
-  _transformPeople = it => {
-    const id = this._extractId(it);
-    const imgUrl = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`;
+  _transformPeople = (it) => {
+    const id = this._extractId(it)
+    const imgUrl = `https://starwars-visualguide.com/assets/img/characters/${id}.jpg`
 
     return {
       id: id,
@@ -29,58 +29,58 @@ export default class SwapiService {
       mass: it.mass,
       name: it.name,
       birthYear: it.birth_year,
-      imgUrl: imgUrl
-    };
-  };
+      imgUrl: imgUrl,
+    }
+  }
 
   getAllPlanets = async () => {
-    const res = await this.getResource('planets/');
-    return res.results.map(this._transformPlanet);
-  };
+    const res = await this.getResource('planets/')
+    return res.results.map(this._transformPlanet)
+  }
 
-  getPlanet = async id => {
-    const planet = await this.getResource(`planets/${id}/`);
-    return this._transformPlanet(planet);
-  };
+  getPlanet = async (id) => {
+    const planet = await this.getResource(`planets/${id}/`)
+    return this._transformPlanet(planet)
+  }
 
-  _extractId = it => {
-    const idRegExp = /\/([0-9]*)\/$/;
-    const id = it.url.match(idRegExp)[1];
-    return id;
-  };
+  _extractId = (it) => {
+    const idRegExp = /\/([0-9]*)\/$/
+    const id = it.url.match(idRegExp)[1]
+    return id
+  }
 
-  _transformPlanet = it => {
-    const id = this._extractId(it);
-    const imgUrl = `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`;
+  _transformPlanet = (it) => {
+    const id = this._extractId(it)
+    const imgUrl = `https://starwars-visualguide.com/assets/img/planets/${id}.jpg`
     return {
       id: this._extractId(it),
       name: it.name,
       population: it.population,
       rotationPeriod: it.rotation_period,
       diameter: it.diameter,
-      imgUrl: imgUrl
-    };
-  };
+      imgUrl: imgUrl,
+    }
+  }
 
   getAllStarships = async () => {
-    const res = await this.getResource('starships/');
-    return res.results.map(this._transformStarship);
-  };
+    const res = await this.getResource('starships/')
+    return res.results.map(this._transformStarship)
+  }
 
-  getStarship = async id => {
-    const res = await this.getResource(`starships/${id}/`);
-    return this._transformStarship(res);
-  };
+  getStarship = async (id) => {
+    const res = await this.getResource(`starships/${id}/`)
+    return this._transformStarship(res)
+  }
 
-  _transformStarship = it => {
-    const id = this._extractId(it);
-    const imgUrl = `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`;
+  _transformStarship = (it) => {
+    const id = this._extractId(it)
+    const imgUrl = `https://starwars-visualguide.com/assets/img/starships/${id}.jpg`
     return {
       id: id,
       name: it.name,
       model: it.model,
       maxSpeed: it.max_atmosphering_speed,
-      imgUrl: imgUrl
-    };
-  };
+      imgUrl: imgUrl,
+    }
+  }
 }
